c271a066d2c2015b35192e56642dafd39cc99973
Raphael Galdino <raphael.galdino.s@hotmail.com>
2026-04-07 18:56:32 -0300

    build: add swagger and audit fix

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -4,12 +4,17 @@ import groupRoutes from "../infrastructure/http/routes/group.routes";
 import movieRoutes from "../infrastructure/http/routes/movie.routes";
 import voteRoutes from "../infrastructure/http/routes/vote.routes";
 import { prismaService } from '../infrastructure/database/prisma/client/prisma.service';
+import swaggerUi from "swagger-ui-express";
+import openapiSpecification from '../docs/swagger';
 
 const app = express();
 app.use(express.json());
 app.use(groupRoutes);
 app.use(movieRoutes);
 app.use(voteRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
